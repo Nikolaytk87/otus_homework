2de985f8559ad1172e9acd8a09ef75b93a951151
Nikolaytk87 <42153211+Nikolaytk87@users.noreply.github.com>
2021-03-13 22:47:28 +0300

    Done homework_1

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,11 +4,12 @@
 """
 
 
-def power_numbers():
+def power_numbers(*args):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     """
+    return [num ** 2 for num in args]
 
 
 # filter types
@@ -17,9 +18,29 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def filter_numbers(list_of_nums, filter_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
     """
+    result_numbers = []
+    if filter_type == 'odd':
+        result_numbers = [num for num in list_of_nums if num % 2 != 0]
+    elif filter_type == 'even':
+        result_numbers = [num for num in list_of_nums if num % 2 == 0]
+    elif filter_type == 'prime':
+        result_numbers = [num for num in list_of_nums if is_prime(num) and num > 1]
+    return result_numbers
+
+
+def is_prime(num):
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    result = filter_numbers([1, 4, 6, 8, 946, 3, 2, 13, 14, 19, 25], 'odd')
+    print(result)
